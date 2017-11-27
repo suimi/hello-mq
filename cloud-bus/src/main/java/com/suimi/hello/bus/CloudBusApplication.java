c355584b50e1bd6bcebdccbc9a04468b93af56cd
suimi <lichengcai125@gmail.com>
2017-11-27 11:59:21 +0800

    spring cloud bus

---
+++ cloud-bus/src/main/java/com/suimi/hello/bus/CloudBusApplication.java
@@ -0,0 +1,12 @@
+package com.suimi.hello.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBusApplication.class, args);
+	}
+}
